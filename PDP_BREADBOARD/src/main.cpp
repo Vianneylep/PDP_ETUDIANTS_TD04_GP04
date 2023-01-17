c0ee3dedceba2deafe04f5e83127e4e10b4fd916
vianney.portalier <vianney.portalier@gmail.com>
2023-01-17 09:04:39 +0100

    etape_2

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+#include <DHT.h>
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+
+ #define DHTPIN 3
+
+ #define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  dht.begin();
 }
 
 void loop() {
-  // put your code here to run repeatedly
+
+ Serial.println("Temperature = " + String(dht.readTemperature())+" °C");
+  Serial.println("Humidite = " + String(dht.readHumidity())+" %");
+  delay(5000);
 }
